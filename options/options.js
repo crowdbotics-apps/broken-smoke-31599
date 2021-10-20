56d4eb9c5b81efdd2de822d43781b797e5927653
Crowdbotics <team@crowdbotics.com>
2021-10-20 17:23:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "broken_smoke_31599",
+  url: "https://broken_smoke_31599.botics.co",
+  api: "https://broken_smoke_31599.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
